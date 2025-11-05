f5e0a21da266496b14be304f95e6ae41f12ce106
jonyoke <jonyoke@gmail.com>
2025-11-05 13:59:51 -0700

    added new class files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 def main():
@@ -10,17 +11,25 @@ def main():
     print("Pygame Initialization")
     print(f"Modules Passed: {pygame_module_init_pass}, Modules Failed: {pygame_module_init_fail}")
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    main_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:         #GAME LOOP
+        #print("Entering Game Loop")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        screen.fill((0,0,0))
+        screen.fill(color="black")
+        main_player.draw(screen)
 
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
