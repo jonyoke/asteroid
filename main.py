25f706397a6439f7cd6c2f21b3ae425c2abb874f
jonyoke <jonyoke@gmail.com>
2025-11-10 23:14:04 -0500

    added asteroid.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from player import Player
+from logger import log_state
 from constants import *
 
 def main():
     print("Starting Asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
     pygame_module_init_pass, pygame_module_init_fail = pygame.init()
     print("Pygame Initialization")
     print(f"Modules Passed: {pygame_module_init_pass}, Modules Failed: {pygame_module_init_fail}")
@@ -14,18 +17,30 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Player is the name of the class, not an instance of it
+    # This must be done before any Player objects are created
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+
     main_player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:         #GAME LOOP
+        log_state()
+
         #print("Entering Game Loop")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
-        main_player.update(dt)
+        updatable.update(dt)
+        #main_player.update(dt)
 
         screen.fill(color="black")
-        main_player.draw(screen)
+        for drawble in drawable:
+            drawble.draw(screen)
+        #main_player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
